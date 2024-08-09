bb99b07ea4363d986213809d783c101767e7defb
ktakahosei <takahiro.kubo.5t@stu.hosei.ac.jp>
2024-08-09 17:07:12 +0900

    ver2

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
-const express = require('express');
+//必要なモジュールのインポート
+const express = require('express');//express:Node.js用のWebフレームワークで、WebアプリケーションやAPIを簡単に構築できる
 const app = express();
-const path = require('node:path');
-const { MongoClient } = require('mongodb');
-const client = new MongoClient('mongodb://localhost:27017');
+const path = require('node:path');//path:ファイルパスを操作するためのNode.jsの標準モジュール
+const { MongoClient } = require('mongodb');//MongoDB:MongoDBのクライアントライブラリで、MongoDBに接続するために使用
+const client = new MongoClient('mongodb://localhost:27017');//client:MongoDBへの接続を管理するインスタンス
 
+
+
+//動的なHTMLページのテンプレートエンジンを設定
+//app.set('.ejsファイルが存在するディレクトリ engine', 'ejs')
 app.set('view engine', 'ejs');
-// publicディレクトリ以下のファイルを静的ファイルとして配信
+//'public'ディレクトリ内のファイルを、'/static'パスで静的ファイルとして配信
 app.use('/static', express.static(path.join(__dirname, 'public')));
-
+//ログミドルウェアの定義
+//logMiddlewareはリクエストのメソッドとパスをコンソールにログ出力します。次のミドルウェアまたはルートハンドラーに処理を渡すために、next()を呼び出します。
 const logMiddleware = (req, res, next) => {
   console.log(req.method, req.path);
   next();
 }
-
+//ユーザーIDの取得
+//'/user/:id'というエンドポイントに対して、リクエストが来ると、logMiddlewareを通過し、リクエストパラメータのidをレスポンスとして送信します。
 app.get('/user/:id', logMiddleware, (req, res) => {
   // :idをreq.params.idとして受け取る
   res.status(200).send(req.params.id);
 });
-
+//エラーハンドリングミドルウェア
+//サーバーでエラーが発生した場合、このミドルウェアが呼ばれ、エラーをコンソールに出力し、500ステータスコードとともに「Internal Server Error」というメッセージをクライアントに返します。
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).send('Internal Server Error');
 });
 
-async function main() {
-  await client.connect();
 
+
+//メイン処理
+async function main() {
+  //MongoDBに接続し'my-app'データベースを選択
+  await client.connect();
   const db = client.db('my-app');
 
+  //ルートハンドラーの設定
+  //GETリクエストが来た場合、userコレクションからすべてのユーザーを取得し、その名前をテンプレートエンジンを使ってindex.ejsファイルでレンダリングする
   app.get('/', logMiddleware, async (req, res) => {
     // const users = ['alpha', 'beta', 'gamma'];
     const users = await db.collection('user').find().toArray();
@@ -37,7 +50,7 @@ async function main() {
 
     res.render(path.resolve(__dirname, 'views/index.ejs'), { users: names });
   });
-
+  //POSTTリクエストが来た場合、リクエストボディからnameを取得し、userコレクションに新しいドキュメントを追加します。名前が提供されていない場合は、400ステータスコードで「Bad Request」を返す
   app.post('/api/user', express.json(), async (req, res) => {
     const name = req.body.name;
     if (!name) {
@@ -48,6 +61,7 @@ async function main() {
     res.status(200).send('Created');
   });
 
+  //サーバー起動
   // ポート: 3000でサーバーを起動
   app.listen(3000, () => {
     // サーバー起動後に呼び出されるCallback
--- public/index.js
+++ public/index.js
@@ -1,14 +1,23 @@
-// public/index.js
+// public/index.js // クライアントサイド
+
+//イベントリスナー
+//ページが完全に読み込まれた後に実行される関数を指定
 window.addEventListener('DOMContentLoaded', (event) => {
-    document.querySelectorAll('.user-name').forEach((elem) => {
-      elem.addEventListener('click', (event) => {
-        alert(event.target.innerHTML);
-      });
-    });
-  
-    document.querySelector('.send-button').addEventListener('click', (event) => {
-      const text = document.querySelector('.input-text').value;
-      fetch('/api/user', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ name: text }) })
+  //クラス名が'user-name'であるすべての要素を選択
+  //forEachメソッドで、各要素に対してクリックイベントリスナーを追加
+  document.querySelectorAll('.user-name').forEach((elem) => {
+    //クリックイベントが発生すると、event.target.innerHTML（クリックされた要素の内部HTML）を取得し、alertでその内容を表示します
+    elem.addEventListener('click', (event) => {
+      alert(event.target.innerHTML);
     });
   });
+  //クラス名がsend-buttonの要素を選択
+  //クリックイベントリスナーが追加されているため、クリックされたときの処理を行う
+  document.querySelector('.send-button').addEventListener('click', (event) => {
+    //クラス名が'input-text'の入力フィールドの値を取得
+    const text = document.querySelector('.input-text').value;
+    //fetch関数を使って、/api/userエンドポイントに対してPOSTリクエストを送信します。リクエストの設定として、Content-Typeをapplication/jsonに設定し、リクエストボディとして{ name: text }というJSONデータを送ります。このデータは、入力フィールドから取得したユーザー名です
+    fetch('/api/user', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ name: text }) })
+  });
+});
   
